73953ecaf438d6120202f5ef7def546365e9cda4
GHOST0O7 <ssr.22u10038@btech.nitdgp.ac.in>
2025-01-16 20:45:21 +0530

    added throttle

--- audition/settings.py
+++ audition/settings.py
@@ -192,6 +192,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',  # By default, all views are accessible
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/hour',
+        'user': '1000/hour',
+    },
 }
 
 SIMPLE_JWT = {
